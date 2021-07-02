d30a4f0abd56edde96b27ece24678f6370d6a172
Ashley Pridgeon <ashley.pridgeon@bristol.ac.uk>
2021-07-02 20:50:47 +0100

    Add files via upload

---
+++ src/gameBackground.js
@@ -0,0 +1,22 @@
+export function setGameBackground(t)
+{
+    if(t.data.values.waterLevel < 10){
+        t.gameBackground.setFillStyle(0x9b2226, 0.2);
+    }
+    if(t.data.values.waterLevel >= 20 && t.data.values.waterLevel < 30){
+        t.gameBackground.setFillStyle(0xbb3e03, 0.2);
+    }
+    if(t.data.values.waterLevel >= 30 && t.data.values.waterLevel < 40){
+        t.gameBackground.setFillStyle(0xee9b00, 0.2);
+        t.data.values.gameBegun = true;
+    }
+    if(t.data.values.waterLevel >= 40 && t.data.values.waterLevel < 50){
+        t.gameBackground.setFillStyle(0x89d8a6, 0.2);
+    }
+    if(t.data.values.waterLevel >= 50 && t.data.values.waterLevel < 60){
+        t.gameBackground.setFillStyle(0x94d2bd, 0.2);
+    }
+    if(t.data.values.waterLevel >= 60 && t.data.values.waterLevel < 70){
+        t.gameBackground.setFillStyle(0x0a9396, 0.2);
+    }
+}
