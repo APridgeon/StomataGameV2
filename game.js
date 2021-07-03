49f39eaedff5ac9bd56c7a0e8a599d163a614479
Ashley Pridgeon <ashley.pridgeon@bristol.ac.uk>
2021-07-03 13:38:40 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 import  TitleScene  from "./scenes/TitleScene.js";
 import  MainScene  from "./scenes/MainScene.js";
 import InstructScene from "./scenes/InstructScene.js";
+import PointsScene from "./scenes/PointsScene.js";
 
 //testing mobile
 var isMobile = (window.innerWidth < 500 || window.innerHeight < 500) ? true : false;
@@ -23,7 +24,7 @@ var config = {
             debug: false
         }
     },
-    scene: [TitleScene, InstructScene, MainScene],
+    scene: [TitleScene, InstructScene, MainScene, PointsScene],
     parent: 'test'
 };
 
